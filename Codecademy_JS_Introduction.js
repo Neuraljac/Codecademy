0ad61ce1cfa31de84cab0207914466a0db2dd220
Jared F. King <101604891+Neuraljac@users.noreply.github.com>
2023-12-21 11:15:12 -0500

    Update Codecademy_JS_Introduction.js

--- Codecademy_JS_Introduction.js
+++ Codecademy_JS_Introduction.js
@@ -26,3 +26,9 @@ console.log('Hello' + 'World');
 console.log('Hello ' + 'World');
 
 console.log('Teaching the world how to code'.length);
+
+// Use .toUpperCase() to log 'Codecademy' in all uppercase letters
+console.log('Codecademy'.toUpperCase());
+
+// Use a string method to log the following string without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   '.trim());
