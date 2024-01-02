cf3aced74ff5500ebf36544fab117bde2bc00613
Jared F. King <101604891+Neuraljac@users.noreply.github.com>
2024-01-02 13:57:06 -0500

    Update Codecademy_JS_Introduction.js

--- Codecademy_JS_Introduction.js
+++ Codecademy_JS_Introduction.js
@@ -64,3 +64,8 @@ const entree = 'Enchiladas'
 
 // Just to check that you’ve saved the value of 'Enchiladas' to entree, log the value of entree to the console.
 console.log(entree);
+
+// Great, let’s see what happens if you try to reassign a constant variable. Paste the following code to the bottom of your program: entree = 'Tacos'
+// This code throws the following error when you run your code: TypeError: Assignment to constant variable.
+entree = 'Tacos';
+
