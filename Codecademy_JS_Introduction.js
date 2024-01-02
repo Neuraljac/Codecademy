cc269d0f8bb61af43e45e151062cd5c4d257ab1d
Jared F. King <101604891+Neuraljac@users.noreply.github.com>
2024-01-02 12:50:53 -0500

    Update Codecademy_JS_Introduction.js

--- Codecademy_JS_Introduction.js
+++ Codecademy_JS_Introduction.js
@@ -40,3 +40,5 @@ console.log(Math.floor(Math.random() * 100));
 // Find a method on the JavaScript Math object that returns the smallest integer greater than or equal to a decimal number. Use this method with the number 43.8. Log the answer to the console.
 console.log(Math.ceil(43.8));
 
+// Use the JavaScript documentation to find a method on the built-in Number object that checks if a number is an integer. Put the number 2017 in the parentheses of the method and use console.log() to print the result.
+console.log(Number.isInteger(2017));
