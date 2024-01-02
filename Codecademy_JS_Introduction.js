c4ea21df19e2d1dd6a2556b988a390dccb02bb2d
Jared F. King <101604891+Neuraljac@users.noreply.github.com>
2024-01-02 12:48:36 -0500

    Update Codecademy_JS_Introduction.js

--- Codecademy_JS_Introduction.js
+++ Codecademy_JS_Introduction.js
@@ -36,3 +36,6 @@ console.log('    Remove whitespace   '.trim());
 
 // Inside of a console.log(), create a random number with Math.random(), then multiply it by 100.
 console.log(Math.floor(Math.random() * 100));
+
+// Now, use Math.floor() to make the output a whole number. Inside the console.log() you wrote in the last step, put the existing Math.random() * 100 code inside the parentheses of Math.floor().
+console.log(Math.ceil(43.8));
