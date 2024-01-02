03ff115aac002319a8cab2981a4f6bbabfad6738
Jared F. King <101604891+Neuraljac@users.noreply.github.com>
2024-01-02 14:18:02 -0500

    Update Codecademy_JS_Introduction.js

--- Codecademy_JS_Introduction.js
+++ Codecademy_JS_Introduction.js
@@ -81,7 +81,7 @@ let quarterMe = 1152;
 // Use the mathematical assignments in the space below:
 levelUp += 5;
 powerLevel -= 100;
-
+multiplyMe *= 11;
 
 
 // These console.log() statements below will help you check the values of the variables.
