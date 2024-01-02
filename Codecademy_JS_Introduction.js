766bae0ddb3aff5d3d0a57dcb97f59c8a3cc8744
Jared F. King <101604891+Neuraljac@users.noreply.github.com>
2024-01-02 13:34:38 -0500

    Update Codecademy_JS_Introduction.js

--- Codecademy_JS_Introduction.js
+++ Codecademy_JS_Introduction.js
@@ -47,3 +47,6 @@ var favoriteFood = 'pizza'
 
 // Declare a variable named numOfSlices using the var keyword and assign to it the number 8.
 var numOfSlices = 8
+
+// Under the numOfSlices variable, use console.log() to print the value saved to favoriteFood. On the following line, use console.log() to print the value saved to numOfSlices.
+console.log(numOfSlices)
