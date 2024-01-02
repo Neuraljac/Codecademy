ec8a92d9076a7059230582b9f4e43f48acfa0795
Jared F. King <101604891+Neuraljac@users.noreply.github.com>
2024-01-02 12:50:00 -0500

    Update Codecademy_JS_Introduction.js

--- Codecademy_JS_Introduction.js
+++ Codecademy_JS_Introduction.js
@@ -34,8 +34,9 @@ console.log('Codecademy'.toUpperCase());
 console.log('    Remove whitespace   '.trim());
 
 
-// Inside of a console.log(), create a random number with Math.random(), then multiply it by 100.
+// Inside of a console.log(), create a random number with Math.random(), then multiply it by 100. // Now, use Math.floor() to make the output a whole number. Inside the console.log() you wrote in the last step, put the existing Math.random() * 100 code inside the parentheses of Math.floor().
 console.log(Math.floor(Math.random() * 100));
 
-// Now, use Math.floor() to make the output a whole number. Inside the console.log() you wrote in the last step, put the existing Math.random() * 100 code inside the parentheses of Math.floor().
+// Find a method on the JavaScript Math object that returns the smallest integer greater than or equal to a decimal number. Use this method with the number 43.8. Log the answer to the console.
 console.log(Math.ceil(43.8));
+
