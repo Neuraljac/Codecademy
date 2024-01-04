9705e2cec8195c6de0c9dcde6cf713ce2f2b586f
Jared F. King <101604891+Neuraljac@users.noreply.github.com>
2024-01-04 12:33:54 -0500

    Update Codecademy_JS_Introduction.js

--- Codecademy_JS_Introduction.js
+++ Codecademy_JS_Introduction.js
@@ -98,3 +98,8 @@ gainedDollar ++;
 let lostDollar = 50;
 lostDollar --;
 
+// Create a variable named favoriteAnimal and set it equal to your favorite animal.
+let favoriteAnimal = 'octopus';
+
+// Use console.log() to print 'My favorite animal: ANIMAL' to the console. Use string concatenation so that ANIMAL is replaced with the value in your favoriteAnimal variable.
+console.log('My favorite animal: ' + favoriteAnimal);
