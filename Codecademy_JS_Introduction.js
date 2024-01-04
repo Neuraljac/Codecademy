6029a220b8d1b31df068116e5936b713e4c2fed6
Jared F. King <101604891+Neuraljac@users.noreply.github.com>
2024-01-04 12:45:29 -0500

    Update Codecademy_JS_Introduction.js

--- Codecademy_JS_Introduction.js
+++ Codecademy_JS_Introduction.js
@@ -103,3 +103,12 @@ let favoriteAnimal = 'octopus';
 
 // Use console.log() to print 'My favorite animal: ANIMAL' to the console. Use string concatenation so that ANIMAL is replaced with the value in your favoriteAnimal variable.
 console.log('My favorite animal: ' + favoriteAnimal);
+
+// Create a variable called myName and assign it your name.
+const myName = 'Jared'
+
+// Create a variable called myCity and assign it your favorite city’s name.
+let myCity = 'New Haven'
+
+// Use a single template literal to interpolate your variables into the sentence below. Use console.log() to print your sentence to the console in the following format: My name is NAME. My favorite city is CITY. Replace NAME and CITY in the string above by interpolating the values saved to myName and myCity.
+console.log(`My name is ${myName}. My favorite city is ${myCity}`);
