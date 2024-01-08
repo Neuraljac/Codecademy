77d4a64e5087cdeb55b98aa41e7e26c0640e6a35
Jared F. King <101604891+Neuraljac@users.noreply.github.com>
2024-01-08 12:05:15 -0500

    Update Codecademy_JS_Introduction.js

--- Codecademy_JS_Introduction.js
+++ Codecademy_JS_Introduction.js
@@ -138,3 +138,14 @@ if (sale) {
   console.log('Time to buy!');
 } // Does not print to console.
 
+// Add an else statement to the existing if statement. Inside the code block of the else statement, console.log() the string 'Time to wait for a sale.'
+let sale = true;
+
+sale = false;
+
+if(sale) {
+  console.log('Time to buy!');
+} else {
+  console.log('Time to wait for a sale.');
+}
+
