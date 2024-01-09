cb9f059badae45049ad327c26d7640251864c0d8
Jared F. King <101604891+Neuraljac@users.noreply.github.com>
2024-01-09 13:44:40 -0500

    Update Codecademy_JS_Introduction.js

--- Codecademy_JS_Introduction.js
+++ Codecademy_JS_Introduction.js
@@ -357,4 +357,11 @@ function sayThanks(name) {
 // A customer named Cole just purchased something from your online store. Call sayThanks() and pass 'Cole' as an argument to send Cole a personalized thank you message.
 sayThanks('Cole');
 
+// The function makeShoppingList() creates a shopping list based on the items that are passed to the function as arguments. Imagine that you always purchase milk, bread, and eggs every time you go shopping for groceries. To make creating a grocery list easier, let’s assign default values to the parameters in makeShoppingList(). Change the parameters of makeShoppingList() into default parameters : Assign ‘milk’ as the default value of item1. Assign ‘bread’ as the default value of item2. Assign ‘eggs’ as the default value of item3.
+function makeShoppingList(item1 = 'milk', item2 = 'bread', item3 = 'eggs'){
+  console.log(`Remember to buy ${item1}`);
+  console.log(`Remember to buy ${item2}`);
+  console.log(`Remember to buy ${item3}`);
+}
+
 // 
