f46c635096d8f0e352207a479e397032973ceed4
Jared F. King <101604891+Neuraljac@users.noreply.github.com>
2024-01-09 14:40:08 -0500

    Update Codecademy_JS_Introduction.js

--- Codecademy_JS_Introduction.js
+++ Codecademy_JS_Introduction.js
@@ -405,7 +405,32 @@ const totalCost = costOfMonitors(5, 4);
 // To check that the function worked properly, log totalCost to the console.
 console.log(totalCost);
 
+// Let’s say we have a plant that we need to water once a week on Wednesdays. We could define a function expression to help us check the day of the week and if the plant needs to be watered: Create a variable named plantNeedsWater using the const variable keyword Assign an anonymous function that takes in a parameter of day to plantNeedsWater.
+const plantNeedsWater = function(day){
+  
+}
 
+// Now we need to add some code to the function body of plantNeedsWater(): In the function body add an if conditional that checks day === 'Wednesday'. If the conditional is truthy, inside the if code block, use the return keyword to return true.
+const plantNeedsWater = function(day){
+  if (day === 'Wednesday') {
+    return true;
+  }
+}
+
+// On days that aren’t 'Wednesday', plantNeedsWater() should return false: Add an else statement after the if statement. Inside the else statement use the return keyword to return false.
+const plantNeedsWater = function(day){
+  if (day === 'Wednesday') {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Call the plantNeedsWater() and pass in 'Tuesday' as an argument.
+plantNeedsWater('Tuesday');
+
+// Let’s check that plantNeedsWater() returned the expected value. Log plantNeedsWater('Tuesday') to the console. If it worked correctly, you should see false logged to the console.
+console.log(plantNeedsWater('Tuesday'));
 
 
 
