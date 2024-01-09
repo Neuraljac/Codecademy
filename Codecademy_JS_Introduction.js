d7c76f5d59eaf82d79b62ef90fbcbc98255f5bc3
Jared F. King <101604891+Neuraljac@users.noreply.github.com>
2024-01-09 14:18:43 -0500

    Update Codecademy_JS_Introduction.js

--- Codecademy_JS_Introduction.js
+++ Codecademy_JS_Introduction.js
@@ -379,3 +379,38 @@ const numOfMonitors = monitorCount(5, 4);
 
 // To check that the function worked properly, log numOfMonitors to the console.
 console.log(numOfMonitors);
+
+
+// In the previous exercise, we created a function to find the number of monitors to order for an office. Now let’s write another function that uses the monitorCount function to figure out the price. Below monitorCount Create a function declaration named costOfMonitors that has two parameters, the first parameter is rows and the second parameter is columns. Leave the function body empty for now.
+function monitorCount(rows, columns) {
+  return rows * columns;
+}
+
+function costOfMonitors(rows, columns) {
+  
+}
+
+// Time to add some code to the function body of costOfMonitors to calculate the total cost. Add a return statement that returns the value of calling monitorCount(rows, columns) multiplied by 200.
+function monitorCount(rows, columns) {
+  return rows * columns;
+}
+
+function costOfMonitors(rows, columns) {
+  return monitorCount(rows, columns) * 200;
+}
+
+// We should save the cost to a variable. Declare a variable named totalCost using the const keyword. Assign to totalCost the value of calling costOfMonitors() with the arguments 5 and 4 respectively.
+const totalCost = costOfMonitors(5, 4);
+
+// To check that the function worked properly, log totalCost to the console.
+console.log(totalCost);
+
+
+
+
+
+
+
+
+
+
