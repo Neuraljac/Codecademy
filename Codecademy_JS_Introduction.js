c4faac1c43981c15b96841116d356a6a6435f669
Jared F. King <101604891+Neuraljac@users.noreply.github.com>
2024-01-12 13:27:15 -0500

    Update Codecademy_JS_Introduction.js

--- Codecademy_JS_Introduction.js
+++ Codecademy_JS_Introduction.js
@@ -509,8 +509,45 @@ console.log(callMyNightSky());
 // Outside the function, under the current console.log() statement, add another console.log() statement to log stars to the console. You’ll notice that the global variable stars was reassigned to 'Sirius'. In other words, we changed the value of the global stars variable but it’s not easy to read what exactly happened. This is bad practice in code maintainability and could impact our program in ways we do not intend.
 console.log(stars);
 
+// Inside the function body of logVisibleLightWaves(), beneath the region variable and before the provided console.log() statement, create an if statement that checks if the region is the 'The Arctic'.
+const logVisibleLightWaves = () => {
+  let lightWaves = 'Moonlight';
+	let region = 'The Arctic';
+  // Add if statement here:
+  if (region === 'The Arctic') {
+    
+  }
+  console.log(lightWaves);
+};
 
+logVisibleLightWaves();
 
+// Inside the if block, define a new let variable lightWaves and set it equal to 'Northern Lights'.
+const logVisibleLightWaves = () => {
+  let lightWaves = 'Moonlight';
+	let region = 'The Arctic';
+  // Add if statement here:
+  if (region === 'The Arctic') {
+    let lightWaves = 'Northern Lights';
+  }
+  console.log(lightWaves);
+};
+
+logVisibleLightWaves();
+
+// Beneath the variable in the if block, use console.log() to log the value of the block variable inside the if block. Run your code and notice the output. Inside the if block console.log(lightWaves) logs the value Northern Lights to the console. Outside the if block, but still within the function, the same statement logs Moonlight to the console.
+const logVisibleLightWaves = () => {
+  let lightWaves = 'Moonlight';
+	let region = 'The Arctic';
+  // Add if statement here:
+  if (region === 'The Arctic') {
+    let lightWaves = 'Northern Lights';
+    console.log(lightWaves);
+  }
+  console.log(lightWaves);
+};
+
+logVisibleLightWaves();
 
 
 
