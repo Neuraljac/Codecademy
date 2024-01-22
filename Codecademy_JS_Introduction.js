336a1a44f92a205591ee040b5074e585faf3d281
Jared F. King <101604891+Neuraljac@users.noreply.github.com>
2024-01-22 09:45:43 -0500

    Update Codecademy_JS_Introduction.js

--- Codecademy_JS_Introduction.js
+++ Codecademy_JS_Introduction.js
@@ -588,6 +588,40 @@ console.log(condiments);
 utensils[3] = 'Spoon';
 console.log(utensils);
 
+// Find the length of the objectives array and log it to the console.
+const objectives = ['Learn a new language', 'Read 52 books', 'Run a marathon'];
+console.log(objectives.length);
+
+// 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
