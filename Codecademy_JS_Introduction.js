9fb1f7ad74bd87e480b84781d3b157ed7af28963
Jared F. King <101604891+Neuraljac@users.noreply.github.com>
2024-01-29 10:29:25 -0500

    Update Codecademy_JS_Introduction.js

--- Codecademy_JS_Introduction.js
+++ Codecademy_JS_Introduction.js
@@ -655,6 +655,15 @@ const numberClusters = [[1, 2], [3, 4], [5, 6]];
 const target = numberClusters[2][1];
 
 
+// Create the variable vacationSpots, and assign its value to an array of three strings naming places you’d like to visit.
+let vacationSpots = ['string1', 'string2', 'string3'];
+
+// Next, console.log() each item in vacationSpots. Since we don’t know loops yet, we have to console.log() each element in the array separately.
+console.log(vacationSpots[0]);
+console.log(vacationSpots[1]);
+console.log(vacationSpots[2]);
+
+// Nice work! Now imagine that the vacation list had 100 places on it— logging each array element to the console by hand would be a tedious task! In the next exercise, we will learn how to make things more efficient with for loops.
 
 
 
