cad8199c558b903469b45322d8e8601383c50b07
Jared F. King <101604891+Neuraljac@users.noreply.github.com>
2024-01-29 11:04:48 -0500

    Update Codecademy_JS_Introduction.js

--- Codecademy_JS_Introduction.js
+++ Codecademy_JS_Introduction.js
@@ -681,9 +681,23 @@ for (let i = 0; i < vacationSpots.length; i++) {
   console.log(`I would love to visit ${vacationSpots[i]}`);
 }
 
+// Imagine you’re a big-wig programmer for a social media platform! You have been tasked with building a prototype for a mutual followers program. You’ll need two arrays of “friends” from two mock users so that you can extract the names of the followers who exist in both lists. Make a variable called bobsFollowers and set it equal to an array with four strings representing the names of Bob’s friends.
+const bobsFollowers = ['follower1', 'follower2', 'follower3', 'follower4'];
 
+// Make a variable called tinasFollowers and set it equal to an array with three strings representing the names of Tina’s friends. Make exactly two of these the same as two of the friends in the bobsFollowers array.
+const tinasFollowers = ['Jimmothy', 'follower2', 'Betty'];
 
+// Create a third variable named mutualFollowers and set it to an empty array.
+const mutualFollowers = [];
 
+// Create a nested loop that iterates through bobsFollowers as the array for the outer loop and tinasFollowers as the array for the inner loop. If the current element from the outer loop is the same as the current element from the inner loop, push that element into the mutualFollowers array.
+for (let i = 0; i < bobsFollowers.length; i++) {
+  for (let j = 0; j < tinasFollowers.length; j++) {
+    if (bobsFollowers[i] === tinasFollowers[j]) {
+      mutualFollowers.push(bobsFollowers[i]);
+    }
+  }
+};
 
 
 
