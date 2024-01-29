756672c83b1824771d779794c8c5b75fe746f82b
Jared F. King <101604891+Neuraljac@users.noreply.github.com>
2024-01-29 11:16:42 -0500

    Update Codecademy_JS_Introduction.js

--- Codecademy_JS_Introduction.js
+++ Codecademy_JS_Introduction.js
@@ -699,7 +699,19 @@ for (let i = 0; i < bobsFollowers.length; i++) {
   }
 };
 
+// Below the cards array, declare a variable, currentCard, with the let keyword but don’t assign it a value.
+let currentCard;
 
+// Create a while loop with a condition that checks if the currentCard does not have that value 'spade'. Inside the block of your while loop, add the following line of code: currentCard = cards[Math.floor(Math.random() * 4)]; Math.floor(Math.random() * 4) will give us a random number from 0 to 3. We’ll use this number to index the cards array, and assign the value of currentCard to a random element from that array. If you notice the Run button spinning continuously or a “Lost connection to Codecademy” message in an exercise, you may have an infinite loop! If the stop condition for our loop is never met, we will create an infinite loop which stops our program from running anything else. To exit out of an infinite loop in an exercise, refresh the page — then fix the code for your loop.
+while (currentCard != 'spade') {
+  currentCard = cards[Math.floor(Math.random() * 4)];
+}
+
+// Awesome! Your loop is running, but you can’t tell because it doesn’t output anything. Let’s add a console.log() statement to our while block. Inside the block, after you assign currentCard a new value, log currentCard to the console. For fun you can run your code a few times and see how the output changes!
+while (currentCard != 'spade') {
+  currentCard = cards[Math.floor(Math.random() * 4)];
+  console.log(currentCard);
+}
 
 
 
