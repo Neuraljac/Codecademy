1d37aeb9a479611ce2141d896bb9284195a44785
Jared F. King <101604891+Neuraljac@users.noreply.github.com>
2024-01-31 11:26:59 -0500

    Update Codecademy_JS_Introduction.js

--- Codecademy_JS_Introduction.js
+++ Codecademy_JS_Introduction.js
@@ -743,6 +743,11 @@ for (let i = 0; i < rapperArray.length; i++){
 console.log("And if you don't know, now you know.");
 
 
+// The spaceship we have so far looks good but, unfortunately, is not very fast at hyperspace travel due to having an inferior fuel source. Make a new spaceship object named fasterShip with the same color as spaceship but with a Fuel Type equal to 'Turbo Fuel'.
+let fasterShip = {
+  color: 'silver',
+  'Fuel Type': 'Turbo Fuel'
+};
 
 
 
