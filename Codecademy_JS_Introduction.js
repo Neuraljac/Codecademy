a4da37555d8f718baa73557340bb6591bdac4913
Jared F. King <101604891+Neuraljac@users.noreply.github.com>
2024-01-31 13:38:53 -0500

    Update Codecademy_JS_Introduction.js

--- Codecademy_JS_Introduction.js
+++ Codecademy_JS_Introduction.js
@@ -794,7 +794,28 @@ spaceship.numEngines = 6;
 // Use the delete operator to remove the 'Secret Mission' property from the spaceship object.
 delete spaceship['Secret Mission'];
 
+// Below the retreatMessage variable in the code editor, create an alienShip object. It should contain a method retreat() which will console.log() the retreatMessage.
+let retreatMessage = 'We no longer wish to conquer your planet. It is full of dogs, which we do not care for.';
 
+const alienShip = {
+  retreat () {
+    console.log(retreatMessage);
+  }
+}
+
+// Add another method to your object literal. This method, takeOff(), should console.log() the string 'Spim... Borp... Glix... Blastoff!'.
+const alienShip = {
+  retreat() {
+    console.log(retreatMessage);
+  },
+  takeOff() {
+    console.log('Spim... Borp... Glix... Blastoff!');
+  }
+}
+
+// Invoke your two methods: first .retreat() then .takeOff().
+alienShip.retreat();
+alienShip.takeOff();
 
 
 
