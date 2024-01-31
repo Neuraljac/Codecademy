053c4d6237420d7c90f6471beb673a67950408b2
Jared F. King <101604891+Neuraljac@users.noreply.github.com>
2024-01-31 11:32:01 -0500

    Update Codecademy_JS_Introduction.js

--- Codecademy_JS_Introduction.js
+++ Codecademy_JS_Introduction.js
@@ -749,6 +749,21 @@ let fasterShip = {
   'Fuel Type': 'Turbo Fuel'
 };
 
+// Let’s use the dot operator to access the value of numCrew from the spaceship object in the code editor. Create a variable crewCount and assign the spaceship‘s numCrew property to it.
+let spaceship = {
+  homePlanet: 'Earth',
+  color: 'silver',
+  'Fuel Type': 'Turbo Fuel',
+  numCrew: 5,
+  flightPath: ['Venus', 'Mars', 'Saturn']
+};
+
+// Write your code below
+let crewCount = spaceship.numCrew;
+
+// Again using the dot operator, create a variable planetArray and assign the spaceship‘s flightPath property to it.
+let planetArray = spaceship.flightPath;
+
 
 
 
