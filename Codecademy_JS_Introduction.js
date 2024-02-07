26e4cb372746c53fb2ce3c3ae3772a5423de9f40
Jared F. King <101604891+Neuraljac@users.noreply.github.com>
2024-02-07 13:50:02 -0500

    Update Codecademy_JS_Introduction.js

--- Codecademy_JS_Introduction.js
+++ Codecademy_JS_Introduction.js
@@ -995,6 +995,57 @@ robot._energyLevel = 'high';
 // Now take a look at the new recharge method in robot. .recharge() will add 30 to _energyLevel. What will happen now that _energyLevel isn’t a number? Call .recharge() on robot to find out. Notice that a funky string is printed to the console! This is known as a side-effect of type-coercion. No need to worry about what this means for now, but it’s important to understand that you can cause unwanted side-effects when mutating objects and their properties.
 robot.recharge();
 
+// In robot, create a getter method named energyLevel using the get keyword. Leave the function body blank for now.
+const robot = {
+  _model: '1E78V2',
+  _energyLevel: 100,
+  get energyLevel() {
+    
+  }
+};
+
+// Inside the getter method, add an if statement to check if this._energyLevel is a number using the typeof operator. If that condition is truthy, return 'My current energy level is ENERGYLEVEL'. Replace ENERGYLEVEL with the value of this._energyLevel. Make sure you return the string instead of logging it to the console.
+const robot = {
+  _model: '1E78V2',
+  _energyLevel: 100,
+  get energyLevel() {
+    if (typeof this._energyLevel === 'number'){
+      return `My current energy level is ${this._energyLevel}`;
+    } 
+  }
+};
+
+// If this._energyLevel isn’t a number it could be that the _energyLevel property was altered. Let’s add a default return statement for when such a scenario arises. Add an else statement that returns 'System malfunction: cannot retrieve energy level'.
+const robot = {
+  _model: '1E78V2',
+  _energyLevel: 100,
+  get energyLevel() {
+    if (typeof this._energyLevel === 'number'){
+      return `My current energy level is ${this._energyLevel}`;
+    } else {
+      return 'System malfunction: cannot retrieve energy level'
+    }
+  }
+};
+
+// Log the result of calling the getter method energyLevel on robot to the console. Notice that the method will return a formatted response rather than just accessing a property!
+console.log(robot.energyLevel);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
