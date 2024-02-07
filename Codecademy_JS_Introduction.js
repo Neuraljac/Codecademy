458a3de3dbfd4f2f902153aa8d2b4365b5dbffa1
Jared F. King <101604891+Neuraljac@users.noreply.github.com>
2024-02-07 13:17:47 -0500

    Update Codecademy_JS_Introduction.js

--- Codecademy_JS_Introduction.js
+++ Codecademy_JS_Introduction.js
@@ -971,8 +971,15 @@ const robot = {
 // Now to check .provideInfo() has access to the internal properties of robot. Log the result of calling .provideInfo() method on robot to the console.
 console.log(robot.provideInfo());
 
+// Currently the .checkEnergy() method is not producing the correct output because it is using arrow function syntax. Refactor, or change, the method to use a function expression. You can write the function using either longhand or shorthand format. After refactoring the method, notice that .checkEnergy() has access to the other internal properties of the robot object.
+const robot = {
+  energyLevel: 100,
+  checkEnergy() {
+    console.log(`Energy is currently at ${this.energyLevel}%.`)
+  }
+}
 
-
+robot.checkEnergy();
 
 
 
