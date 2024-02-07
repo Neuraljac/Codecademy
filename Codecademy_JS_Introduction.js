dd43d169a63fc72060d6ee23d631b03f35041ec2
Jared F. King <101604891+Neuraljac@users.noreply.github.com>
2024-02-07 13:25:07 -0500

    Update Codecademy_JS_Introduction.js

--- Codecademy_JS_Introduction.js
+++ Codecademy_JS_Introduction.js
@@ -981,6 +981,19 @@ const robot = {
 
 robot.checkEnergy();
 
+// Below the robot object, reassign the _energyLevel property to 'high'.
+const robot = {
+  _energyLevel: 100,
+  recharge(){
+    this._energyLevel += 30;
+    console.log(`Recharged! Energy is currently at ${this._energyLevel}%.`)
+  }
+};
+
+robot._energyLevel = 'high';
+
+// Now take a look at the new recharge method in robot. .recharge() will add 30 to _energyLevel. What will happen now that _energyLevel isn’t a number? Call .recharge() on robot to find out. Notice that a funky string is printed to the console! This is known as a side-effect of type-coercion. No need to worry about what this means for now, but it’s important to understand that you can cause unwanted side-effects when mutating objects and their properties.
+robot.recharge();
 
 
 
