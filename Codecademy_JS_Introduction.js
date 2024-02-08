b781fd3534eee2d31327b0e5e9b7626ac8d2dcac
Jared F. King <101604891+Neuraljac@users.noreply.github.com>
2024-02-08 10:30:12 -0500

    Update Codecademy_JS_Introduction.js

--- Codecademy_JS_Introduction.js
+++ Codecademy_JS_Introduction.js
@@ -1031,15 +1031,68 @@ const robot = {
 // Log the result of calling the getter method energyLevel on robot to the console. Notice that the method will return a formatted response rather than just accessing a property!
 console.log(robot.energyLevel);
 
+// Currently, in robot there is a getter method for numOfSensors but no setter method! What if we need to add or remove some sensors? Let’s fix that problem. Add a setter method named numOfSensors using the set keyword. Provide a parameter of num. Leave the function body empty for now.
+const robot = {
+  _model: '1E78V2',
+  _energyLevel: 100,
+  _numOfSensors: 15,
+  get numOfSensors(){
+    if(typeof this._numOfSensors === 'number'){
+      return this._numOfSensors;
+    } else {
+      return 'Sensors are currently down.'
+    }
+  },
+  set numOfSensors(num) {
+    
+  }
+};
 
+// There are a couple of things we should do in the setter method: Add a check to see if num is a number using the typeof operator. num should also be greater than or equal to 0. If both conditions are met, reassign this._numOfSensors to num.
+const robot = {
+  _model: '1E78V2',
+  _energyLevel: 100,
+  _numOfSensors: 15,
+  get numOfSensors(){
+    if(typeof this._numOfSensors === 'number'){
+      return this._numOfSensors;
+    } else {
+      return 'Sensors are currently down.'
+    }
+  },
+  set numOfSensors(num) {
+    if (typeof num === 'number' && num >= 0) {
+      this._numOfSensors = num;
+    }
+  }
+};
 
+// Now add an else that logs 'Pass in a number that is greater than or equal to 0' to the console.
+const robot = {
+  _model: '1E78V2',
+  _energyLevel: 100,
+  _numOfSensors: 15,
+  get numOfSensors(){
+    if(typeof this._numOfSensors === 'number'){
+      return this._numOfSensors;
+    } else {
+      return 'Sensors are currently down.'
+    }
+  },
+  set numOfSensors(num) {
+    if (typeof num === 'number' && num >= 0) {
+      this._numOfSensors = num;
+    } else {
+      return 'Pass in a number that is greater than or equal to 0';
+    }
+  }
+};
 
+// Use the numOfSensors setter method on robot to assign _numOfSensors to 100.
+robot.numOfSensors = 100;
 
-
-
-
-
-
+// To check that the setter method worked, console.log() robot.numOfSensors.
+console.log(robot.numOfSensors);
 
 
 
