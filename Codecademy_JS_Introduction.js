6f928ac312b989f88ce91675f1550f8a11382c8e
Jared F. King <101604891+Neuraljac@users.noreply.github.com>
2024-02-14 12:31:26 -0500

    Update Codecademy_JS_Introduction.js

--- Codecademy_JS_Introduction.js
+++ Codecademy_JS_Introduction.js
@@ -1189,9 +1189,14 @@ const newRobot = Object.assign({laserBlaster: true, voiceRecognition: true}, rob
 console.log(newRobot);
 
 
+// We’ve defined a function with a very long name: checkThatTwoPlusTwoEqualsFourAMillionTimes(). This function takes a long time to execute. It checks whether 2 + 2 = 4, but it does it a million times (just to be really sure)! Using const, declare a shorter-named variable, isTwoPlusTwo that will be easier to work with. Assign checkThatTwoPlusTwoEqualsFourAMillionTimes as its value.
+const isTwoPlusTwo = checkThatTwoPlusTwoEqualsFourAMillionTimes;
 
+// Invoke your isTwoPlusTwo() function.
+isTwoPlusTwo();
 
-
+// Hmmm, if we forgot the original name of our function. Is there a way we could figure it out? Use isTwoPlusTwo to console.log() the name property of the function we assigned to isTwoPlusTwo.
+console.log(isTwoPlusTwo.name)
 
 
 
