8b6e0894148d1b7474ca22c9ffdc6b77b3917199
Jared F. King <101604891+Neuraljac@users.noreply.github.com>
2024-02-16 12:07:41 -0500

    Update Codecademy_JS_Introduction.js

--- Codecademy_JS_Introduction.js
+++ Codecademy_JS_Introduction.js
@@ -1237,6 +1237,28 @@ const checkConsistentOutput = (func, val) => {
 
 console.log(checkConsistentOutput(addTwo, 10));
 
+// Inspect the code in main.js. Notice the different methods being called on the arrays: .forEach() .map() .filter() Run the code to see the output!
+const artists = ['Picasso', 'Kahlo', 'Matisse', 'Utamaro'];
+
+artists.forEach(artist => {
+  console.log(artist + ' is one of my favorite artists.');
+});
+
+const numbers = [1, 2, 3, 4, 5];
+
+const squareNumbers = numbers.map(number => {
+  return number * number;
+});
+
+console.log(squareNumbers);
+
+const things = ['desk', 'chair', 5, 'backpack', 3.14, 100];
+
+const onlyNumbers = things.filter(thing => {
+  return typeof thing === 'number';
+});
+
+console.log(onlyNumbers);
 
 
 
