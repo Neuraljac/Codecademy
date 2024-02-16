2305a0c56ccee581dc0019a979b377ed886c2503
Jared F. King <101604891+Neuraljac@users.noreply.github.com>
2024-02-16 12:44:37 -0500

    Update Codecademy_JS_Introduction.js

--- Codecademy_JS_Introduction.js
+++ Codecademy_JS_Introduction.js
@@ -1260,8 +1260,12 @@ const onlyNumbers = things.filter(thing => {
 
 console.log(onlyNumbers);
 
+// Iterate over the fruits array to log I want to eat a plus the name of each fruit to the console. For example, I want to eat a mango. You may use any form of callback you prefer.
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
 
-
+// Iterate over fruits below
+fruits.forEach(fruit =>
+  console.log(`I want to eat a ${fruit}`));
 
 
 
