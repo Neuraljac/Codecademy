ee08ce77e3c15448e9a2be0d4b97d7c709e9f555
Jared F. King <101604891+Neuraljac@users.noreply.github.com>
2024-02-26 13:31:15 -0500

    Update Codecademy_JS_Introduction.js

--- Codecademy_JS_Introduction.js
+++ Codecademy_JS_Introduction.js
@@ -1325,8 +1325,18 @@ const newSum = newNumbers.reduce((accumulator, currentValue) => {
 
 console.log(newSum);
 
+// In the code editor, there is an array called words. We want to create a new array of interesting words. The first thing we want to do is check if there are words that are fewer than 6 characters long. There is something missing in the words.some() method call. Fix this method so that true is printed to the console.
+const words = ['unique', 'uncanny', 'pique', 'oxymoron', 'guise'];
+// Something is missing in the method call below
+console.log(words.some(word => {
+  return word.length < 6;
+}));
 
+// The .some() method returned true, which means there are some words in the array that are shorter than six characters. Use the .filter() method to save any words longer than 5 characters to a new variable called interestingWords, declared with const.
+const interestingWords = words.filter((word) => {return word.length > 5});
 
+// Complete the code between the parentheses to check if every element has more than 5 characters.
+console.log(interestingWords.every((word) => {return word.length > 5}));
 
 
 
